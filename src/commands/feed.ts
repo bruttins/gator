e7a90b3b3b03e213df522af075425eb81ff50649
bruttins <bruttins@gmail.com>
2026-05-08 15:51:57 +0200

    add aggregator loop

--- src/commands/feed.ts
+++ src/commands/feed.ts
@@ -1,17 +1,67 @@
 import { fetchFeed } from '../lib/rss';
-import { createFeed, getFeeds } from '../lib/db/queries/feeds';
+import { createFeed, getFeeds, getNextFeedToFetch, markFeedFetched } from '../lib/db/queries/feeds';
 import { Feed, User } from '../lib/db/schema';
 import { createFeedFollow } from '../lib/db/queries/feed-follows';
 
 
 export async function handlerAgg(cmdName: string, ...args: string[]) {
-    try {
-        const feed = await fetchFeed('https://www.wagslane.dev/index.xml');
-        console.log(JSON.stringify(feed, null, 2));
-        }
-    catch (error) {
-        console.error(`Error fetching feed: ${(error as Error).message}`);
+    if (args.length !== 1) {
+        throw new Error(`Usage: agg <time_between_reqs>`);
     }
+    const timeBetweenRequests = parseDuration(args[0]);
+    console.log(`Fetching feeds every ${args[0]}`);
+    scrapeFeeds().catch((err) => {
+        console.error(err);
+    });
+
+    const interval = setInterval(() => {
+        scrapeFeeds().catch((err) => {
+            console.error(err);
+        });
+    }, timeBetweenRequests);
+    await new Promise<void>((resolve) => {
+        process.on("SIGINT", () => {
+            console.log("Shutting down feed aggregator...");
+            clearInterval(interval);
+            resolve();
+        });
+    });
+}
+
+function parseDuration(durationStr: string): number {
+    const regex = /^(\d+)(ms|s|m|h)$/;
+    const match = durationStr.match(regex);
+    if (!match) {
+        throw new Error(`Invalid duration format: ${durationStr}`);
+    }
+    const value = parseInt(match[1], 10);
+    const unit = match[2];
+    switch (unit) {
+        case 'ms':
+            return value;
+        case 's':
+            return value * 1000;
+        case 'm':
+            return value * 60 * 1000;
+        case 'h':
+            return value * 60 * 60 * 1000;
+        default:
+            throw new Error(`Unsupported duration unit: ${unit}`);
+    }
+}
+
+async function scrapeFeeds() {
+    const feedToFetch = await getNextFeedToFetch();
+    if (!feedToFetch) {
+        console.log('No feeds to fetch');
+        return;
+    }
+    await markFeedFetched(feedToFetch.id);
+    const feedData = await fetchFeed(feedToFetch.url);
+    console.log(`Fetched feed: ${feedToFetch.name} (${feedToFetch.url})`);
+    for (const item of feedData.channel.item) {
+        console.log(item.title);
+    };
 }
 
 export async function handlerAddFeed(cmdName: string, user: User, ...args: string[]) {
--- src/lib/db/queries/feeds.ts
+++ src/lib/db/queries/feeds.ts
@@ -1,6 +1,6 @@
 import { db } from "..";
 import { feeds, users } from "../schema";
-import { eq } from "drizzle-orm";
+import { eq, sql } from "drizzle-orm";
 
 export async function createFeed(name: string, url: string, userId: string) {
   const [result] = await db.insert(feeds).values({ name, url, userId }).returning();
@@ -19,3 +19,17 @@ export async function getFeedByUrl(url: string) {
   const [result] = await db.select().from(feeds).where(eq(feeds.url, url));
   return result;
 }
+
+export async function markFeedFetched(feedId: string) {
+  await db.update(feeds).set({
+    lastFetchedAt: new Date(),
+    updatedAt: new Date()
+  }
+  ).where(eq(feeds.id, feedId));
+}
+
+export async function getNextFeedToFetch() {
+  const [result] = await db.select().from(feeds).orderBy(sql`${feeds.lastFetchedAt} ASC NULLS FIRST`).limit(1);
+  return result;
+}
+
